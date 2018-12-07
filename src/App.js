9819725adcf2b86952ec49c91e135342f6a54618
Melissa Lewis <mlewis@heliotraining.com>
2018-12-07 14:28:17 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import Application from './Components/Application';
+
+class App extends Component {
+    render() {
+        return(
+            <div>
+                <Application/>
+            </div>
+        )
+    }
+}
+export default App
